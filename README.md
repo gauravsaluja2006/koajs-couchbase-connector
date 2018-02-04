1d1201ce75a5ed7e7614955f7592e612427ffa9e
Gaurav Saluja <gauravsaluja2006@gmail.com>
2018-02-04 10:39:55 +0530

    Initial Module

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # koajs-couchbase-connector
 Integrate Couchbase with Koa JS to access all your buckets in your middleware 
+
+=========
+
+## Installation
+
+  `npm install koajs-couchbase-connector`
+
+## Usage
+
+    const Koa = require('koa');
+    const couchOperations = require('koajs-couchbase-connector');
+
+    const app = new Koa();
+
+    // COUCHBASE SETTINGS
+    const couchBaseOptions = {
+        connections: [
+            {
+                bucket: 'sample-bucket1'
+            },
+            {
+                bucket: 'sample-bucket2'
+            }
+        ],
+        host: "localhost",
+        username: "Administrator",
+        password: "mypassword"
+    }
+
+    app.use(createCouchbaseConnections(couchBaseOptions));
+
+    app.use(async ctx => {
+        ctx.couchbase['sample-bucket1'].get('key', function (err, result) {
+            // ....
+        });
+    })
+
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Any contributions are most welcome.
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Integrate Couchbase with Koa JS 
+ * to access all your buckets in your middleware
+ * @param {object} options
+ * 
+ * Example Options:
+ * 
+ * const couchBaseOptions = {
+ *   connections: [
+ *       {
+ *           bucket: 'sample-bucket1'
+ *       }
+ *   ],
+ *   host: "localhost",
+ *   username: "Administrator",
+ *   password: "mypassword"
+ * }
+ * 
+ */
+module.exports = createCouchbaseConnections;
+
+function createCouchbaseConnections(options) {
+
+    let connectionUrl = `couchbase://${options.host}`;
+    
+    var cluster = new couchbase.Cluster();
+    cluster.authenticate(options.username, options.password);
+
+    // this will contain all the bucket references
+    let connections = {}
+
+    // Will be creating the Bucket Instances once here
+    options.connections.map((settings) => {
+        connections[settings.bucket] = cluster.openBucket(settings.bucket);
+    })
+
+    // middleware function to attach all the opened buckets 
+    // to the Request Context every time
+    return async (ctx, next) => {
+        ctx.couchbase = connections;
+        await next();
+    };
+}
